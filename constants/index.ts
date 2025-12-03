86899cd60196e4316987b2e56c5767d60d9ca708
O-Gui-Carvalho <guilhermefreitas117@gmail.com>
2025-12-03 19:45:57 -0300

    Photos window implemented

--- constants/index.ts
+++ constants/index.ts
@@ -526,3 +526,61 @@ export const socials = [
     link: "https://www.linkedin.com/company/javascriptmastery/posts/?feedView=all",
   },
 ];
+
+export interface PhotosLinks {
+  id: number;
+  icon: string;
+  title: string;
+}
+
+export const photosLinks: PhotosLinks[] = [
+  {
+    id: 1,
+    icon: "/icons/gicon1.svg",
+    title: "Library",
+  },
+  {
+    id: 2,
+    icon: "/icons/gicon2.svg",
+    title: "Memories",
+  },
+  {
+    id: 3,
+    icon: "/icons/file.svg",
+    title: "Places",
+  },
+  {
+    id: 4,
+    icon: "/icons/gicon4.svg",
+    title: "People",
+  },
+  {
+    id: 5,
+    icon: "/icons/gicon5.svg",
+    title: "Favorites",
+  },
+];
+
+export interface Gallery {
+  id: number;
+  img: string;
+}
+
+export const gallery: Gallery[] = [
+  {
+    id: 1,
+    img: "/images/gal1.png",
+  },
+  {
+    id: 2,
+    img: "/images/gal2.png",
+  },
+  {
+    id: 3,
+    img: "/images/gal3.png",
+  },
+  {
+    id: 4,
+    img: "/images/gal4.png",
+  },
+];
--- windows/Photos.tsx
+++ windows/Photos.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+'use client'
+
+import { WindowControlls } from '@/components'
+import { photosLinks, gallery } from '@/constants'
+import WindowsWrapper from '@/hoc/WindowsWrapper'
+import { Search } from 'lucide-react'
+import Image from 'next/image'
 
 const Photos = () => {
+
   return (
-    <div>Photos</div>
+    <>
+      <div id="window-header">
+        <WindowControlls target='photos' />
+        <Search className='icon' />
+      </div>
+
+      <div className="bg-white flex h-full">
+        <div className="sidebar">
+          <h3>Photos</h3>
+          
+          <ul>
+            {(photosLinks.map((item) => (
+              <li key={item.id}>
+                <Image 
+                  src={item.icon} 
+                  alt={item.title} 
+                  width={16} 
+                  height={16}
+                />
+
+                <p className="text-sm font-medium">
+                  {item.title}
+                </p>
+              </li>
+            )))}
+          </ul>
+        </div>
+
+        <div className="gallery">
+          <ul>
+            {gallery.map((item) => (
+              <li key={item.id}>
+                <Image 
+                  src={item.img} 
+                  alt={`Gallery ${item.id}`}
+                  width={500}
+                  height={500}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Photos
+const PhotosWindow = WindowsWrapper(Photos, 'photos')
+
+export default PhotosWindow
